a7e6f5103df98d3ad4c71a6c5abf7389f0a2eaf1
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-10-01 07:54:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import  from "./Components/AllPost";
+import AllPost from "./Components/AllPost";
 import Create from "./Components/Create";
 import CreatePost from "./Components/CreatePost";
 import Navbar from "./Components/Navbar";
